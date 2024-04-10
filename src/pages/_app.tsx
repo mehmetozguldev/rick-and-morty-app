c98e55f85cb704080ca98e31efa90d694221ff16
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2024-04-10 20:25:36 +0300

    Fonts added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/lib/store";
+import { Josefin_Sans, Montserrat } from "next/font/google";
+import Header from "@/components/Header";
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  variable: "--font-josefin-sans",
+});
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <main className={`${montserrat.variable} ${josefinSans.variable}`}>
+        <Header />
+        <Component {...pageProps} />
+      </main>
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
-import { Josefin_Sans, Montserrat } from "next/font/google";
-
-const josefinSans = Josefin_Sans({ subsets: ["latin"] });
-const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${josefinSans.className}`}
-    >
-      Home
-    </main>
-  );
+  return <main>Home</main>;
 }
