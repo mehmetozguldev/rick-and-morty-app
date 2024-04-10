c5cc85637bfed16ab09ff23ccd524efd0134590b
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2024-04-10 18:49:51 +0300

    Store created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${josefinSans.className}`}
     >
-Home
+      Home
     </main>
   );
 }
