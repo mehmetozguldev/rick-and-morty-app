c82152e520f9ce62c267b94f72da5dbd26649ff8
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2024-04-15 09:42:28 +0300

    Added isActive feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
+  const router = useRouter();
+  const isActive = (path: string) => {
+    if (path === "/") {
+      return router.asPath === "/";
+    } else {
+      return router.asPath.startsWith(path);
+    }
+  };
+
   return (
     <Navbar>
       <Container fluid className="justify-content-start">
@@ -17,12 +27,24 @@ const Header = () => {
             />
           </Link>
         </Navbar.Brand>
-        <Nav activeKey="/" className="font-josefin text-uppercase">
+        <Nav activeKey={router.asPath} className="font-josefin text-uppercase">
           <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link
+              style={{ color: isActive("/") ? "#42aa01" : undefined }}
+              href="/"
+            >
+              Home
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/locations">Locations</Nav.Link>
+            <Nav.Link
+              style={{
+                color: isActive("/locations") ? "#42aa01" : undefined,
+              }}
+              href="/locations"
+            >
+              Locations
+            </Nav.Link>
           </Nav.Item>
         </Nav>
         <Link href="/my-favourites" className="ms-auto">
