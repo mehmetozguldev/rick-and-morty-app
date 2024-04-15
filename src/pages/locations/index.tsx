0548be748d54e79c5a3c020429cf5a315e57fd42
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2024-04-15 09:43:13 +0300

    Layout fixed

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -1,10 +1,7 @@
 import AllLocations from "@/components/Locations/AllLocations";
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export default function page() {
-  return (
-    <div>
-      <AllLocations />
-    </div>
-  );
+  return <AllLocations />;
 }
